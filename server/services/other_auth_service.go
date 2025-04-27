8d4ae9c2bd5fd84e1aaf2fefe0313bcedf49147a
liuhaifeng <liuhaifeng@sensetime.com>
2025-04-27 10:18:04 +0800

    upgrade(other):

--- server/services/other_auth_service.go
+++ server/services/other_auth_service.go
@@ -12,23 +12,18 @@ type otherAuthService struct{}
 
 var OtherAuthService = otherAuthService{}
 
-func (svc otherAuthService) AuthenticateService(path string, headers map[string][]string) (bool, map[string]string) {
+func (svc otherAuthService) AuthenticateService(path string, headers map[string][]string) map[string]string {
 	headerJson, _ := json.MarshalIndent(headers, "", "    ")
 	logger.Log.Info(string(headerJson))
 
 	// panic(globals.NewException("other auth service not implemented"))
-	return true, map[string]string{"path": path, "auth": "other"}
+	return map[string]string{"path": path, "auth": "other"}
 }
 
 // ============================================ gin接口处理函数 ============================================
 func (svc otherAuthService) OtherAuth(c *gin.Context) {
 	path := c.Param("path")
-	ok, data := svc.AuthenticateService(path, c.Request.Header)
-
-	if !ok {
-		response.Ctx(c).Failed("Authentication failed")
-		return
-	}
+	data := svc.AuthenticateService(path, c.Request.Header)
 
 	response.Ctx(c).Success(data)
 }
--- server/services/usermanager_auth_service.go
+++ server/services/usermanager_auth_service.go
@@ -10,34 +10,24 @@ type userManagerAuthService struct{}
 
 var UserManagerAuthService = userManagerAuthService{}
 
-func (svc userManagerAuthService) AuthenticateService(path string, auth string) (bool, map[string]string) {
+func (svc userManagerAuthService) AuthenticateService(path string, auth string) map[string]string {
 
 	// panic(globals.NewException("other auth service not implemented"))
-	return true, map[string]string{"path": path, "auth": auth}
+	return map[string]string{"path": path, "auth": auth}
 }
 
 // ============================================ gin接口处理函数 ============================================
 
 func (svc userManagerAuthService) AdminAuth(c *gin.Context) {
 	path := c.Param("path")
-	ok, data := svc.AuthenticateService(path, "admin验证")
-
-	if !ok {
-		response.Ctx(c).Failed("Authentication failed")
-		return
-	}
+	data := svc.AuthenticateService(path, "admin验证")
 
 	response.Ctx(c).Success(data)
 }
 
 func (svc userManagerAuthService) LoginAuth(c *gin.Context) {
 	path := c.Param("path")
-	ok, data := svc.AuthenticateService(path, "需要登录验证")
-
-	if !ok {
-		response.Ctx(c).Failed("Authentication failed")
-		return
-	}
+	data := svc.AuthenticateService(path, "需要登录验证")
 
 	response.Ctx(c).Success(data)
 }
